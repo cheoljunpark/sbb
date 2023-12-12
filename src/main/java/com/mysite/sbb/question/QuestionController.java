5c973bc2778b367ca473a164469243a401e6b4ce
aruma <dkfnak6789@gmail.com>
2023-12-13 00:44:22 +0900

    feat: map question list url

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,13 +1,22 @@
 package com.mysite.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    private final QuestionService questionService;
+
     @GetMapping("/question/list")
-    public String list(){
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
